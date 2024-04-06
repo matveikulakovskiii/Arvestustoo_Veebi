e4ef1f44806212a11a7d5cc5fbfb476844c27945
matveikulakovskiii <120181570+matveikulakovskiii@users.noreply.github.com>
2024-04-06 21:02:05 +0300

    Update main.php

--- main.php
+++ main.php
@@ -10,7 +10,7 @@ session_start();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tantsud tähtedega</title>
+    <title>Veebipood</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <style>
         body {
