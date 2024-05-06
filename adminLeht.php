b0b6c684691a44dcdb0956fcff99df7d6e757995
Deniss Gorjunov <pinkod2222@gmail.com>
2024-05-06 11:21:56 +0300

    Lisa üks pilt vastavalt teemale

--- adminLeht.php
+++ adminLeht.php
@@ -118,6 +118,7 @@ if(isset($_REQUEST["kustuta2"])){
     }
     ?>
 </header>
+<img src="scale_1200.jpg">
 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
